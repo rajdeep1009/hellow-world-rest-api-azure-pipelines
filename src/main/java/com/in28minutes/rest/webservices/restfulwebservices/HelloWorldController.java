fc9f671e98ae96c1b53dc18a59d806ec5c35d83d
Rajdeep <rajdeep.dutta.96@gmail.com>
2020-06-04 12:02:18 +0530

    second commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -17,7 +17,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World CI CD";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
